72aba5827b164ad958ca565a12818bf44e008598
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-23 07:24:25 +0400

    add validation for FirstName

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ function App() {
     Email: string;
     Password: string;
   };
+  const [isFirstNameValid, setIsFirstNameValid] = useState<boolean>(false);
+  const [isLastNameValid, setIsLastNameValid] = useState<boolean>(false);
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
+  const [isPasswordValid, setPasswordValid] = useState<boolean>(false);
+
   const [lager, setLager] = useState<TUserProps[]>([]);
   const [userInfo, setUserInfo] = useState<TUserProps>({
     FirstName: "",
@@ -33,10 +38,21 @@ function App() {
   };
 
   function checkErrors(updatedUserInfo: TUserProps) {
-    setErrors(prevErrors => ({
+    setErrors((prevErrors) => ({
       ...prevErrors,
-      FirstName: updatedUserInfo.FirstName === "" ? "Input Can't be empty" : "",
+      FirstName:
+        updatedUserInfo.FirstName === ""
+          ? "Input Can't be empty"
+          : updatedUserInfo.FirstName.split("").length < 5
+          ? "Enter at least 5 chars"
+          : "",
     }));
+    {
+      setIsFirstNameValid(
+        updatedUserInfo.FirstName !== "" &&
+          updatedUserInfo.FirstName.length >= 5
+      );
+    }
   }
 
   const handleSubmission = (event: React.FormEvent) => {
@@ -46,44 +62,68 @@ function App() {
     setUserInfo({ FirstName: "", LastName: "", Email: "", Password: "" });
   };
 
+  const handleFirstErrorMessage = (event: React.FormEvent) => {
+    const target = event.target as HTMLInputElement;
+    if (userInfo.FirstName === "") {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [target.name]: "Input Can't be empty",
+      }));
+    }
+  };
+
   return (
     <>
       <form
         onSubmit={handleSubmission}
         className="flex flex-col w-[250px] h-[250px] gap-2"
       >
+        <span>FirstName</span>
         <input
           onChange={handleChange}
+          onClick={handleFirstErrorMessage}
           type="text"
           name="FirstName"
           value={userInfo.FirstName}
-          className="border-[5px] border-solid border-blue-500"
+          className={`border-[2px] rounded-lg p-[3px] border-solid ${
+            isFirstNameValid ? "border-green-600" : "border-red-600"
+          }`}
         />
         {errors.FirstName ? (
           <p className="text-red-600 text-sm">{errors.FirstName}</p>
         ) : null}
+        <span>LastName</span>
         <input
           onChange={handleChange}
           type="text"
           name="LastName"
           value={userInfo.LastName}
-          className="border-[5px] border-solid border-blue-500"
+          className={`border-[2px] rounded-lg p-[3px] border-solid ${
+            isPasswordValid ? "border-green-600" : "border-red-600"
+          }`}
         />
+        <span>Email</span>
         <input
           onChange={handleChange}
           type="text"
           name="Email"
           value={userInfo.Email}
-          className="border-[5px] border-solid border-blue-500"
+          className={`border-[2px] rounded-lg p-[3px] border-solid ${
+            isPasswordValid ? "border-green-600" : "border-red-600"
+          }`}
         />
+        <span>Password</span>
         <input
           onChange={handleChange}
           type="password"
           name="Password"
           value={userInfo.Password}
-          className="border-[5px] border-solid border-blue-500"
+          className={`border-[2px] rounded-lg p-[3px] border-solid ${
+            isPasswordValid ? "border-green-600" : "border-red-600"
+          }`}
         />
-        <button type="submit" className="p-[10px] bg-green-500">
+
+        <button type="submit" className="p-[10px] rounded-lg bg-green-500">
           Submit
         </button>
       </form>
